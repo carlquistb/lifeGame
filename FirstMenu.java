928f006bf6e733fee897c485bd5e6652ae661834
Brendan Carlquist <carlquistb@gmail.com>
2014-12-17 23:19:19 -0600

    first addition
    added everything I had before I made this into a GitHub thing. Right
    now, the main functionality of the program is to create and color a grid
    with a specifiable number of rows and columns. must be a square.

---
+++ FirstMenu.java
@@ -0,0 +1,70 @@
+import org.newdawn.slick.Color;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Input;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.BasicGameState;
+import org.newdawn.slick.state.StateBasedGame;
+@SuppressWarnings("unused")
+
+
+public class FirstMenu extends BasicGameState{
+	
+	public static boolean[][] field = makeNewField(100);           //this parameter is all you need to change to get a different field size. be careful with it, i don't know how it will react if the ints need to round. Or, how it will interact with pre-made fields.
+
+	public FirstMenu(String title) {
+		super();
+	}
+
+	public void init(GameContainer container, StateBasedGame sbg)
+			throws SlickException {
+		
+	}
+	
+	public void update(GameContainer container, StateBasedGame sbg, int delta)
+			throws SlickException {//update game logic here
+		if(container.getInput().isKeyPressed(Input.KEY_ESCAPE)){
+			sbg.enterState(1);
+		}
+		
+	}
+	
+	public void render(GameContainer container, StateBasedGame sbg, Graphics g)
+			throws SlickException {
+		g.setColor(Color.red);
+		int squarexSize = Default.rightEndOfScreen/field.length;
+		int squareySize = Default.bottomEndOfScreen/field.length;
+		//so the problem is that the Life.java thing isn't starting. When referencing variables from it, they variables always come back as just initialized.
+		for(int a = 0; a < field.length; a++)
+			for(int b = 0; b < field.length; b++)
+				if(field[a][b])
+					g.fillRect(a*squarexSize, b*squareySize, squarexSize-1, squareySize-1);
+				else
+					g.drawRect(a*squarexSize, b*squareySize, squarexSize-1, squareySize-1);
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+
+	public int getID() {
+		return 0;
+	}
+	
+	private static boolean[][] makeNewField(int fieldSize) {
+		boolean[][] newField = new boolean[fieldSize][fieldSize];
+		
+		
+		for(int a = 0; a < newField.length; a++)
+			for(int b = 0; b < newField.length; b++)
+				newField[a][b] = true;
+		
+		return newField;
+	}
+}
+	
+	
---
+++ Life.java
@@ -0,0 +1,7 @@
+
+public class Life {
+	public static boolean[][] field = new boolean[100][100];
+	for(int a = 0; a < field.length; a++)
+		for(int b = 0; b < field[a].length; b++)
+			field[a][b] = true;
+}
